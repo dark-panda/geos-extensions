1edfa0fe35e9b2ac331b47ef1a2670b1deccf078
J Smith <jay@zoocasa.com>
2009-04-20 16:05:45 +0000

    Avoid unnecessary SQL queries.
    git-svn-id: file:///usr/local/svnroot/ruby_extensions/geos_extensions/trunk@13422 05ac5844-ee52-4e45-b639-0480b35ff74b

--- lib/active_record_extensions/connection_adapters/postgresql_adapter.rb
+++ lib/active_record_extensions/connection_adapters/postgresql_adapter.rb
@@ -1,42 +1,31 @@
 
 module ActiveRecord
 	module ConnectionAdapters
-		class PostgreSQLColumn < Column
-			attr_accessor :srid, :coord_dimensions
-		end
+		class PostgreSQLGeometryColumn < Struct.new(:name, :srid, :coord_dimension); end
 
 		class PostgreSQLAdapter < AbstractAdapter
 			# Returns the geometry columns for the table.
 			def geometry_columns(table_name, name = nil)
-				columns_with_geometry_detection(table_name, name).select do |c|
-					c.type == :geometry
-				end
-			end
+				columns(table_name, name).select { |c| c.type == :geometry }.collect do |c|
+					res = execute(
+						"SELECT * FROM geometry_columns WHERE f_table_name = #{quote(table_name)} AND f_geometry_column = #{quote(c.name)}",
+						"Geometry column load for #{table_name}"
+					)
 
-			def columns_with_geometry_detection(table_name, name = nil)
-				columns = columns_without_geometry_detection(table_name, name)
-				columns.each do |c|
-					if c.type == :geometry
-						res = execute(
-							"SELECT * FROM geometry_columns WHERE f_table_name = #{quote(table_name)} AND f_geometry_column = #{quote(c.name)}",
-							"Geometry column load for #{table_name}"
-						)
-
+					returning(PostgreSQLGeometryColumn.new(c.name)) do |g|
 						# since we're too stupid at the moment to understand
 						# PostgreSQL schemas, let's just go with this:
 						if res.ntuples == 1
-							geometry_column_idx, coord_dimension_idx, srid_idx =
-								res.fields.index('f_geometry_column'),
+							coord_dimension_idx, srid_idx =
 								res.fields.index('coord_dimension'),
 								res.fields.index('srid')
-							c.srid = res.getvalue(0, srid_idx).to_i
-							c.coord_dimensions = res.getvalue(0, coord_dimension_idx).to_i
+
+							g.srid = res.getvalue(0, srid_idx).to_i
+							g.coord_dimension = res.getvalue(0, coord_dimension_idx).to_i
 						end
 					end
 				end
-				columns
 			end
-			alias_method_chain :columns, :geometry_detection
 		end
 	end
 end
