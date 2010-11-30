fbbb290e493eba117d04df7c77a953e51a8ce150
J Smith <jay@zoocasa.com>
2010-11-30 14:50:05 -0500

    Autoload some Rails files.

---
+++ lib/active_record_extensions.rb
@@ -0,0 +1,9 @@
+
+module Geos
+	module ActiveRecord
+		autoload :GeometryColumns, File.join(GEOS_BASE, %w{ active_record_extensions geometry_columns })
+		autoload :GeospatialScopes, File.join(GEOS_BASE, %w{ active_record_extensions geospatial_scopes })
+	end
+end
+
+
