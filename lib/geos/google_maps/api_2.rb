7348fd66df6020a0861fbb58a59a6b11a1e03c66
J Smith <jay@zoocasa.com>
2011-03-25 18:02:40 -0400

    Move the Google Maps stuff into a separate set of files.
    This is so we can make way for Google Maps API 3-specific code.

---
+++ lib/geos/google_maps/api_2.rb
@@ -0,0 +1,187 @@
+
+module Geos
+  class Geometry
+    # Returns a new GLatLngBounds object with the proper GLatLngs in place
+    # for determining the geometry bounds.
+    def to_g_lat_lng_bounds(options = {})
+      klass = if options[:short_class]
+        'GLatLngBounds'
+      else
+        'google.maps.LatLngBounds'
+      end
+
+      "new #{klass}(#{self.lower_left.to_g_lat_lng(options)}, #{self.upper_right.to_g_lat_lng(options)})"
+    end
+
+    # Returns a String in Google Maps' GLatLngBounds#toString() format.
+    def to_g_lat_lng_bounds_string(precision = 10)
+      "((#{self.lower_left.to_g_url_value(precision)}), (#{self.upper_right.to_g_url_value(precision)}))"
+    end
+
+    # Returns a new GPolyline.
+    def to_g_polyline polyline_options = {}, options = {}
+      self.coord_seq.to_g_polyline polyline_options, options
+    end
+
+    # Returns a new GPolygon.
+    def to_g_polygon polygon_options = {}, options = {}
+      self.coord_seq.to_g_polygon polygon_options, options
+    end
+
+    # Returns a new GMarker at the centroid of the geometry. The options
+    # Hash works the same as the Google Maps API GMarkerOptions class does,
+    # but allows for underscored Ruby-like options which are then converted
+    # to the appropriate camel-cased Javascript options.
+    def to_g_marker marker_options = {}, options = {}
+      klass = if options[:short_class]
+        'GMarker'
+      else
+        'google.maps.Marker'
+      end
+
+      opts = marker_options.inject({}) do |memo, (k, v)|
+        memo[Geos::Helper.camelize(k.to_s)] = v
+        memo
+      end
+
+      "new #{klass}(#{self.centroid.to_g_lat_lng(options)}, #{opts.to_json})"
+    end
+  end
+
+  class CoordinateSequence
+    # Returns a Ruby Array of GLatLngs.
+    def to_g_lat_lng(options = {})
+      klass = if options[:short_class]
+        'GLatLng'
+      else
+        'google.maps.LatLng'
+      end
+
+      self.to_a.collect do |p|
+        "new #{klass}(#{p[1]}, #{p[0]})"
+      end
+    end
+
+    # Returns a new GPolyline. Note that this GPolyline just uses whatever
+    # coordinates are found in the sequence in order, so it might not
+    # make much sense at all.
+    #
+    # The options Hash follows the Google Maps API arguments to the
+    # GPolyline constructor and include :color, :weight, :opacity and
+    # :options. 'null' is used in place of any unset options.
+    def to_g_polyline polyline_options = {}, options = {}
+      klass = if options[:short_class]
+        'GPolyline'
+      else
+        'google.maps.Polyline'
+      end
+
+      poly_opts = if polyline_options[:polyline_options]
+        polyline_options[:polyline_options].inject({}) do |memo, (k, v)|
+          memo[Geos::Helper.camelize(k.to_s)] = v
+          memo
+        end
+      end
+
+      args = [
+        (polyline_options[:color] ? "'#{Geos::Helper.escape_javascript(polyline_options[:color])}'" : 'null'),
+        (polyline_options[:weight] || 'null'),
+        (polyline_options[:opacity] || 'null'),
+        (poly_opts ? poly_opts.to_json : 'null')
+      ].join(', ')
+
+      "new #{klass}([#{self.to_g_lat_lng(options).join(', ')}], #{args})"
+    end
+
+    # Returns a new GPolygon. Note that this GPolygon just uses whatever
+    # coordinates are found in the sequence in order, so it might not
+    # make much sense at all.
+    #
+    # The options Hash follows the Google Maps API arguments to the
+    # GPolygon constructor and include :stroke_color, :stroke_weight,
+    # :stroke_opacity, :fill_color, :fill_opacity and :options. 'null' is
+    # used in place of any unset options.
+    def to_g_polygon polygon_options = {}, options = {}
+      klass = if options[:short_class]
+        'GPolygon'
+      else
+        'google.maps.Polygon'
+      end
+
+      poly_opts = if polygon_options[:polygon_options]
+        polygon_options[:polygon_options].inject({}) do |memo, (k, v)|
+          memo[Geos::Helper.camelize(k.to_s)] = v
+          memo
+        end
+      end
+
+      args = [
+        (polygon_options[:stroke_color] ? "'#{Geos::Helper.escape_javascript(polygon_options[:stroke_color])}'" : 'null'),
+        (polygon_options[:stroke_weight] || 'null'),
+        (polygon_options[:stroke_opacity] || 'null'),
+        (polygon_options[:fill_color] ? "'#{Geos::Helper.escape_javascript(polygon_options[:fill_color])}'" : 'null'),
+        (polygon_options[:fill_opacity] || 'null'),
+        (poly_opts ? poly_opts.to_json : 'null')
+      ].join(', ')
+      "new #{klass}([#{self.to_g_lat_lng(options).join(', ')}], #{args})"
+    end
+  end
+
+  class Point
+    # Returns a new GLatLng.
+    def to_g_lat_lng(options = {})
+      klass = if options[:short_class]
+        'GLatLng'
+      else
+        'google.maps.LatLng'
+      end
+
+      "new #{klass}(#{self.lat}, #{self.lng})"
+    end
+
+    # Returns a new GPoint
+    def to_g_point(options = {})
+      klass = if options[:short_class]
+        'GPoint'
+      else
+        'google.maps.Point'
+      end
+
+      "new #{klass}(#{self.x}, #{self.y})"
+    end
+  end
+
+  class Polygon
+    # Returns a GPolyline of the exterior ring of the Polygon. This does
+    # not take into consideration any interior rings the Polygon may
+    # have.
+    def to_g_polyline polyline_options = {}, options = {}
+      self.exterior_ring.to_g_polyline polyline_options, options
+    end
+
+    # Returns a GPolygon of the exterior ring of the Polygon. This does
+    # not take into consideration any interior rings the Polygon may
+    # have.
+    def to_g_polygon polygon_options = {}, options = {}
+      self.exterior_ring.to_g_polygon polygon_options, options
+    end
+  end
+
+  class GeometryCollection
+    # Returns a Ruby Array of GPolylines for each geometry in the
+    # collection.
+    def to_g_polyline polyline_options = {}, options = {}
+      self.collect do |p|
+        p.to_g_polyline polyline_options, options
+      end
+    end
+
+    # Returns a Ruby Array of GPolygons for each geometry in the
+    # collection.
+    def to_g_polygon polygon_options = {}, options = {}
+      self.collect do |p|
+        p.to_g_polygon polygon_options, options
+      end
+    end
+  end
+end
---
+++ test/google_maps_api_2_test.rb
@@ -0,0 +1,186 @@
+
+$: << File.dirname(__FILE__)
+require 'test_helper'
+
+begin
+  require 'json'
+rescue LoadError
+  # do nothing
+end
+
+require 'geos/google_maps/api_2'
+
+class GoogleMapsApi2Tests < Test::Unit::TestCase
+  include TestHelper
+
+  def initialize(*args)
+    @point = Geos.read(POINT_EWKB)
+    @polygon = Geos.read(POLYGON_EWKB)
+    super(*args)
+  end
+
+  def test_to_g_lat_lng
+    assert_equal("new google.maps.LatLng(10.01, 10.0)", @point.to_g_lat_lng)
+    assert_equal("new GLatLng(10.01, 10.0)", @point.to_g_lat_lng(:short_class => true))
+  end
+
+  def test_to_jsonable
+    assert_equal({
+      :type => "point",
+      :lat => 10.01,
+      :lng => 10.0
+    }, @point.to_jsonable)
+
+    assert_equal({
+      :type => "polygon",
+      :polylines => [{
+        :points => "??_ibE_ibE_~cH_~cH_hgN_hgN~po]~po]",
+        :bounds => {
+          :sw => [ 0.0, 0.0 ],
+          :ne => [ 5.0, 5.0 ]
+        },
+        :levels=>"BBBBB"
+      }],
+      :options => {},
+      :encoded => true
+    }, @polygon.to_jsonable)
+  end
+
+  if defined?(JSON)
+    def test_to_g_polygon
+      assert_equal(
+        "new google.maps.Polygon([new google.maps.LatLng(0.0, 0.0), new google.maps.LatLng(1.0, 1.0), new google.maps.LatLng(2.5, 2.5), new google.maps.LatLng(5.0, 5.0), new google.maps.LatLng(0.0, 0.0)], null, null, null, null, null, null)",
+        @polygon.to_g_polygon
+      )
+
+      assert_equal(
+        "new GPolygon([new GLatLng(0.0, 0.0), new GLatLng(1.0, 1.0), new GLatLng(2.5, 2.5), new GLatLng(5.0, 5.0), new GLatLng(0.0, 0.0)], null, null, null, null, null, null)",
+        @polygon.to_g_polygon({}, :short_class => true)
+      )
+
+      assert_equal(
+        "new google.maps.Polygon([new google.maps.LatLng(0.0, 0.0), new google.maps.LatLng(1.0, 1.0), new google.maps.LatLng(2.5, 2.5), new google.maps.LatLng(5.0, 5.0), new google.maps.LatLng(0.0, 0.0)], '#b00b1e', 5, 0.5, '#b00b1e', null, {\"mouseOutTolerence\":5})",
+        @polygon.to_g_polygon(
+          :stroke_color => '#b00b1e',
+          :stroke_weight => 5,
+          :stroke_opacity => 0.5,
+          :fill_color => '#b00b1e',
+          :polygon_options => {
+            :mouse_out_tolerence => 5
+          }
+        )
+      )
+    end
+
+    def test_to_g_polyline
+      assert_equal(
+        "new google.maps.Polyline([new google.maps.LatLng(0.0, 0.0), new google.maps.LatLng(1.0, 1.0), new google.maps.LatLng(2.5, 2.5), new google.maps.LatLng(5.0, 5.0), new google.maps.LatLng(0.0, 0.0)], null, null, null, null)",
+        @polygon.to_g_polyline
+      )
+
+      assert_equal(
+        "new GPolyline([new GLatLng(0.0, 0.0), new GLatLng(1.0, 1.0), new GLatLng(2.5, 2.5), new GLatLng(5.0, 5.0), new GLatLng(0.0, 0.0)], null, null, null, null)",
+        @polygon.to_g_polyline({}, :short_class => true)
+      )
+
+      assert_equal(
+        "new google.maps.Polyline([new google.maps.LatLng(0.0, 0.0), new google.maps.LatLng(1.0, 1.0), new google.maps.LatLng(2.5, 2.5), new google.maps.LatLng(5.0, 5.0), new google.maps.LatLng(0.0, 0.0)], '#b00b1e', 5, 0.5, {\"mouseOutTolerence\":5})",
+        @polygon.to_g_polyline(
+          :color => '#b00b1e',
+          :weight => 5,
+          :opacity => 0.5,
+          :polyline_options => {
+            :mouse_out_tolerence => 5
+          }
+        )
+      )
+    end
+
+    def test_to_g_marker_long
+      marker = @point.to_g_marker
+
+      lat, lng, json = if marker =~ /^new\s+
+        google\.maps\.Marker\(
+          new\s+google\.maps\.LatLng\(
+            (\d+\.\d+)\s*,\s*
+            (\d+\.\d+)
+          \),\s*
+            ((\{\}))
+        \)
+        /x
+        [ $1, $2, $3 ]
+      end
+
+      assert_in_delta(lng.to_f, 10.00, 0.000001)
+      assert_in_delta(lat.to_f, 10.01, 0.000001)
+      assert_equal(
+        {},
+        JSON.load(json)
+      )
+    end
+
+    def test_to_g_marker_short_class
+      marker = @point.to_g_marker({}, :short_class => true)
+
+      lat, lng, json = if marker =~ /^new\s+
+        GMarker\(
+          new\s+GLatLng\(
+            (\d+\.\d+)\s*,\s*
+            (\d+\.\d+)
+          \),\s*
+            (\{\})
+        \)
+        /x
+        [ $1, $2, $3 ]
+      end
+
+      assert_in_delta(lng.to_f, 10.00, 0.000001)
+      assert_in_delta(lat.to_f, 10.01, 0.000001)
+      assert_equal(
+        {},
+        JSON.load(json)
+      )
+    end
+
+
+    def test_to_g_marker_with_options
+      marker = @point.to_g_marker(
+        :bounce_gravity => 1,
+        :bouncy => true
+      )
+
+      lat, lng, json = if marker =~ /^new\s+
+        google\.maps\.Marker\(
+          new\s+google\.maps\.LatLng\(
+            (\d+\.\d+)\s*,\s*
+            (\d+\.\d+)
+          \),\s*
+            (\{[^}]+\})
+        \)
+        /x
+        [ $1, $2, $3 ]
+      end
+
+      assert_in_delta(lng.to_f, 10.00, 0.000001)
+      assert_in_delta(lat.to_f, 10.01, 0.000001)
+      assert_equal(
+        { "bounceGravity" => 1, "bouncy" => true },
+        JSON.load(json)
+      )
+    end
+
+    def test_to_g_json_point
+      assert_equal(
+        { :coordinates => [ 10.0, 10.01, 0 ] },
+        @point.to_g_json_point
+      )
+    end
+
+    def test_to_g_lat_lon_box
+      assert_equal(
+        { :east => 5.0, :west => 0.0, :north => 5.0, :south => 0.0},
+        @polygon.to_g_lat_lon_box
+      )
+    end
+  end
+end
