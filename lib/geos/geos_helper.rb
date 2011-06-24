5ea2c0208acfa17576280b085116db9a9dd90989
J Smith <jay@zoocasa.com>
2011-06-24 17:25:43 -0400

    Some additional helper methods.

--- lib/geos/geos_helper.rb
+++ lib/geos/geos_helper.rb
@@ -20,7 +20,22 @@ module Geos::Helper
     end
   end
 
-    def self.camelize(lower_case_and_underscored_word, first_letter_in_uppercase = false)
+  def self.escape_json(hash, ignore_keys = [])
+    json = hash.inject([]) do |memo, (k, v)|
+      memo.tap {
+        k = k.to_s
+        memo << if ignore_keys.include?(k)
+          "#{k.to_json}: #{v}"
+        else
+          "#{k.to_json}: #{v.to_json}"
+        end
+      }
+    end
+
+    "{#{json.join(', ')}}"
+  end
+
+  def self.camelize(lower_case_and_underscored_word, first_letter_in_uppercase = false)
     if first_letter_in_uppercase
       lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
     else
@@ -42,6 +57,44 @@ module Geos::Helper
     end
 
     [ xml, options ]
+  end
+
+  # Return a new Hash with all keys converted to camelized Strings.
+  def self.camelize_keys(hash, first_letter_in_uppercase = false)
+    hash.inject({}) do |options, (key, value)|
+      options[camelize(key, first_letter_in_uppercase)] = value
+      options
+    end
+  end
+
+  # Destructively convert all keys to camelized Strings.
+  def camelize_keys!(hash, first_letter_in_uppercase = false)
+    hash.tap {
+      hash.keys.each do |key|
+        unless key.class.to_s == 'String'
+          hash[camelize(key, first_letter_in_uppercase)] = hash[key]
+          hash.delete(key)
+        end
+      end
+    }
+  end
+
+  # Deeply camelize a Hash.
+  def deep_camelize_keys(hash, first_letter_in_uppercase = false)
+    camelize_keys(hash, first_letter_in_upppcase).inject({}) do |memo, (k, v)|
+      memo.tap do
+        if v.is_a? Hash
+          memo[k] = deep_camelize_keys(v, first_letter_in_uppercase)
+        else
+          memo[k] = v
+        end
+      end
+    end
+  end
+
+  # Destructively deeply camelize a Hash.
+  def deep_camelize_keys!(hash, first_letter_in_uppercase = false)
+    hash.replace(deep_camelize_keys(hash, first_letter_in_uppercase))
   end
 
   def self.number_with_precision(number, precision = 6)
